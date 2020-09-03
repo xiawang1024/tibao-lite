d1e66b7ea001b9e1f5694450524220afeb0d3997
xiawang1024 <xiawang1024@gmail.com>
2020-09-03 17:41:26 +0800

    审核列表

--- pages/center/index.js
+++ pages/center/index.js
@@ -7,7 +7,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-    isAdmin:false
+    isAdmin:false,
+    num:''
   },
 
   /**
@@ -17,7 +18,8 @@ Page({
     httpAdmin().then(res => {
       if(res.admin) {
         this.setData({
-          isAdmin:true
+          isAdmin:true,
+          num:res.num
         })
       }
     })
