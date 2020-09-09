0385949999b1a7f8de9bb8b3657fbb07afab112c
xiawang1024 <xiawang1024@gmail.com>
2020-09-09 15:38:42 +0800

    首页ok

--- views/index/index.js
+++ views/index/index.js
@@ -5,14 +5,48 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    schKey:'',
+    tabs: [],
+    activeTab_1: 0,
+    activeTab_2: 1,
+    activeTab_3: 0,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    const tabs = [
+      {
+        title: '技术开发',
+      
+      },
+      {
+        title: '产品解析',
+       
+      },
+      {
+        title: '运营规范',
+        
+      },
+      {
+        title: '营销经验',
+       
+      },
+      {
+        title: '高校大赛',
+       
+      },
+      {
+        title: '营销经验1',
+       
+      },
+      {
+        title: '高校大赛1',
+       
+      },
+    ]
+    this.setData({ tabs })
   },
 
   /**
@@ -62,5 +96,34 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  },
+  schIptHandler(e){
+    console.log(e)
+    this.setData({
+      schKey:e.detail.value
+    })
+  },
+  schHandler() {
+    wx.showToast({
+      icon:"none",
+      title: this.data.schKey,
+    })
+  },
+  onTabClick_1(e){
+    
+    this.setData({
+      activeTab_1:e.detail.index
+    })
+  },
+  onTabClick_2(e){
+    this.setData({
+      activeTab_2:e.detail.index
+    })
+  },
+  onTabClick_3(e){
+    this.setData({
+      activeTab_3:e.detail.index
+    })
+  },
+  
 })
