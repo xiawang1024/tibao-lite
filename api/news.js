8e00fa3c093172e9f598d8677e60ad5c91831ed3
xiawang1024 <xiawang1024@gmail.com>
2020-09-10 11:36:55 +0800

    三级导航调整ok

--- api/news.js
+++ api/news.js
@@ -1,17 +1,36 @@
-const httpNewsList= () => {
-  
+import baseUrl from '../config'
+const httpNewsTypes = () => {
+  return new Promise((resolve,reject) => {
+    wx.request({
+      url: `${baseUrl}/public/category`,
+      method:"GET",
+      success(res) {
+        resolve(res.data)
+      },
+      fail(){
+        reject()
+      }
+    })
+  })
+}
 
+
+const httpNewsList= () => {
   return new Promise((resolve,reject) => {
     wx.request({
       url: 'https://www.fastmock.site/mock/51560081c48b3d7a64bceb48ad4d2ab9/api/api/list',
       method:"GET",
       success:(res) => {
         resolve(res.data)
+      },
+      fail() {
+        reject()
       }
     })
   })
 }
 
 export {
+  httpNewsTypes,
   httpNewsList
 }
--- views/index/index.js
+++ views/index/index.js
@@ -1,6 +1,7 @@
 // views/index/index.js
 
 import {
+  httpNewsTypes,
   httpNewsList
 } from '../../api/news'
 
@@ -11,9 +12,11 @@ Page({
    */
   data: {
     schKey: '',
-    tabs: [],
+    tabs_1:[],
+    tabs_2:[],
+    tabs_3:[],
     activeTab_1: 0,
-    activeTab_2: 1,
+    activeTab_2: 0,
     activeTab_3: 0,
     listData: []
   },
@@ -22,45 +25,16 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    const tabs = [{
-        title: '技术开发',
-
-      },
-      {
-        title: '产品解析',
-
-      },
-      {
-        title: '运营规范',
-
-      },
-      {
-        title: '营销经验',
-
-      },
-      {
-        title: '高校大赛',
-
-      },
-      {
-        title: '营销经验1',
-
-      },
-      {
-        title: '高校大赛1',
-
-      },
-    ]
-    this.setData({
-      tabs
-    })
+   
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-    this.getNewsData()
+  onReady () {
+    this.getNewsTypes().then(() => {
+      this.getNewsData()
+    })    
   },
 
   /**
@@ -117,24 +91,92 @@ Page({
     })
   },
   onTabClick_1(e) {
-
+    let idx = e.detail.index
+    let tabs_1 = this.data.tabs_1
+    let tabs_2 = []
+    if(tabs_1[idx].subcate && tabs_1[idx].subcate.length>0){
+      tabs_2 = tabs_1[idx].subcate
+    }
     this.setData({
-      activeTab_1: e.detail.index
+      activeTab_1: idx,
+      tabs_2
+    },() => {
+      this.getNewsData()
     })
   },
   onTabClick_2(e) {
+    let idx = e.detail.index
+    let tabs_2 = this.data.tabs_2
+    let tabs_3 = []
+    if(tabs_2[idx].subcate && tabs_2[idx].subcate.length>0){
+      tabs_3 = tabs_2[idx].subcate
+    }
     this.setData({
-      activeTab_2: e.detail.index
-    })
+      activeTab_2: idx,
+      tabs_3
+    },() => {
+      this.getNewsData()
+    })    
   },
   onTabClick_3(e) {
+    let idx = e.detail.index
     this.setData({
-      activeTab_3: e.detail.index
+      activeTab_3: idx
+    },() => {
+      this.getNewsData()
+    })
+  },
+
+  getNewsTypes(){
+    return new Promise((resolve,reject) => {
+      httpNewsTypes().then(res => {
+        let {code,data} = res
+        if(code === 0) {
+          this.setData({
+            tabs_1:data.categories
+          })
+          
+          this.flatTabTypes(data.categories[0])
+          resolve()
+        }else {
+          reject()
+        }
+      })
+    })
+    
+  },
+
+  flatTabTypes(tabs_1) {
+    let tabs_2 = []
+    let tabs_3 = []
+    if(tabs_1.subcate && tabs_1.subcate.length>0) {
+      tabs_2 = tabs_1.subcate
+
+      if(tabs_2.subcate && tabs_2.subcate.length>0){
+        tabs_3 = tabs_2.subcate
+      }
+    }
+
+    this.setData({
+      tabs_2,
+      tabs_3
     })
   },
 
   getNewsData(type=1) {
     //type 0:下拉   1：上拉
+    let {tabs_1,tabs_2,tabs_3,activeTab_1,activeTab_2,activeTab_3} = this.data
+    let catid = ''
+    console.log(tabs_3.length,activeTab_3)
+    if(tabs_3.length>0){
+      catid = tabs_3[activeTab_3].catid
+    }else if(tabs_2.length>0) {
+      catid = tabs_2[activeTab_2].catid
+    }else {
+      catid = tabs_1[activeTab_1].catid
+    }
+
+    console.log(catid)
     wx.showLoading({
       title: '努力加载中...',
     })
